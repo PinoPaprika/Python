b5c7720152969af17c2df38607807330f81f7263
PinoPaprika <benjamin.gerbe@gmail.com>
2020-11-17 20:01:10 +0100

    init

---
+++ test.py
@@ -0,0 +1,20 @@
+
+class test():
+    """cette class est une class
+    """
+    def fonction1():
+        """Cette fonction fait quelque chose il me semble
+
+        Returns:
+            int: elle retourn zéro
+        """
+        return 0
+    
+
+    def fonction2():
+        """cette vous dit bonjour
+
+        Returns:
+            string: c'est une chaine de caractères
+        """
+        return "salut"
